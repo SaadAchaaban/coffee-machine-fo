916c93dc3ae612231c8c1601890f131ae6c70731
saad achaaban <saad.achaaban@yahoo.fr>
2024-02-15 17:32:01 +0100

    Task 1: Implement basic UI layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,53 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'CoffeeMachine';
+
+  title = 'ICoffeeMachine';
+  
+  state = {
+    isOn: false,
+    isMakingCoffee: false,
+    waterLevelState: 0,
+    beanFeedState: 0,
+    wasteCoffeeState: 0,
+    waterTrayState: 0,
+  }
+
+  coffeeData = {
+    "14/02/2024": {
+      "firstCupTime": "15:27",
+      "lastCupTime": "15:27",
+      "averageCups": 1
+    },
+    "15/02/2024": {
+      "firstCupTime": "14:54",
+      "lastCupTime": "16:19",
+      "averageCups": 7
+    }
+  };
+
+  coffeeDataArray = Object.entries(this.coffeeData).map(([date, data]) => ({
+    date,
+    ...data
+  }));
+
+  coffeeHourlyData = {
+    "14/02/2024": {
+      "15-16": 1
+    },
+    "15/02/2024": {
+      "14-15": 5,
+      "16-17": 2
+    }
+  };
+
+  coffeeHourlyDataArray = Object.entries(this.coffeeHourlyData).map(([date, hours]) => ({
+    date,
+    hours: Object.entries(hours).map(([hourRange, cups]) => ({hourRange, cups}))
+  }));
+
+  turnOnMachine(): void {}
+  turnOffMachine(): void {}
+  makeCoffee(): void {}
+
 }
